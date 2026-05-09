2539d9961825882420773d91384211b355333627
ronald12q <aleperro104@gmail.com>
2026-05-09 15:25:14 -0600

    simple production fix 2

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { FaHeart, FaRegHeart } from "react-icons/fa"
 import { FaTrash } from "react-icons/fa6"
 import { useFavoriteStore } from "../stores/useFavoritesStore"
